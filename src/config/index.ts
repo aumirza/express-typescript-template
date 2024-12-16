c5b1efc1b53bdbddcb53067d3c0e5b11ed7c2862
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2024-12-16 18:29:11 +0530

    Feat: types for express, middleware example, utils and config , cors

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,17 @@
-export const PORT = process.env.PORT || 5000
+import path from "path";
+import dotenv from "dotenv";
+
+const dotenvConfig = {
+  path: path.resolve(__dirname, "../../.env"),
+};
+dotenv.config(dotenvConfig);
+
+import { normalizePort } from "../utils/normalizePort";
+import { CorsOptions } from "cors";
+
+export const PORT = normalizePort(process.env.PORT || 5000);
+
+export const corsOptions: CorsOptions = {
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  credentials: true,
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,8 @@
-import express from 'express'
+import express from "express";
+import indexController from "../controllers/indexController";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+router.get("/", indexController.home);
 
-export default router
+export default router;
