ed9a4059f81d41f39e8fbe2ed9d8b7afdb26062d
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2024-12-18 14:36:00 +0530

    Feat: configured cors, error handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,41 @@
 import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import createError from "http-errors";
+
+import { corsOptions } from "./config/cors";
 import router from "./routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
+// middlewares
+app.use(logger("dev")); // Log requests to console
+app.use(express.json()); // Parse JSON from the request body
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(cors(corsOptions)); // Enable CORS
+
 // app.use(express.static("public"));
 
-app.use(router)
+// Serve static assets if in production
+// if (process.env.NODE_ENV === "production") {
+//     // Set static folder
+//     app.use(express.static("client/build"));
+
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     });
+// }
+
+// Attaching routers
+app.use(router);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(errorHandler);
 
 export default app;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,11 @@
 import path from "path";
 import dotenv from "dotenv";
+import { normalizePort } from "../utils/normalizePort";
 
 const dotenvConfig = {
   path: path.resolve(__dirname, "../../.env"),
 };
 dotenv.config(dotenvConfig);
 
-import { normalizePort } from "../utils/normalizePort";
-import { CorsOptions } from "cors";
-
 export const PORT = normalizePort(process.env.PORT || 5000);
-
-export const corsOptions: CorsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
-  credentials: true,
-};
+export const APP_SECRET = process.env.APP_SECRET || "secret";
